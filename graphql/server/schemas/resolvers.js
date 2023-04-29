79ef743d6183f81e8f4f78f0972fda2e0162afe6
Brahm V <brahmvanh@gmail.com>
2023-04-29 13:57:59 -0400

    Started resolvers.

--- graphql/server/schemas/resolvers.js
+++ graphql/server/schemas/resolvers.js
@@ -1,3 +1,42 @@
+// BE SURE TO ADD VALIDATION EXPRESSION FOR EMAIL ADDRESS HERE
+// Replacing routers and controllers:
 
+// User controllers:
+// getSingleUser  '/me'
+// createUser '/'
+// login '/'
+// saveBook '/'
+// deleteBook '/books/:bookId'
 
-// BE SURE TO ADD VALIDATION EXPRESSION FOR EMAIL ADDRESS HERE 
+const { Book, User } = require('../models');
+
+const resolvers = {
+	Query: {
+		getSingleUser: async ({ user = null, params }, res) => {
+			const foundUser = await User.findOne({
+				$or: [
+					{ _id: user ? user._id : params.id },
+					{ username: params.username },
+				],
+			});
+
+			if (!foundUser) {
+				return res
+					.status(400)
+					.json({ message: 'Cannot find a user with this id!' });
+			}
+
+			return foundUser;
+		},
+    createUser: ({ body }, res) => {
+      const user = await User.create(body);
+
+    if (!user) {
+      return res.status(400).json({ message: 'Something is wrong!' });
+    }
+    const token = signToken(user);
+    return { token, user };
+    }
+	},
+  
+};
