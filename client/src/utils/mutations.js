47f352a6e014d9ed14f998313712ba52acb83a23
Brahm V <brahmvanh@gmail.com>
2023-05-01 14:57:53 -0400

    We're getting somewhere.

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -3,21 +3,11 @@ import { gql } from '@apollo/client';
 export const CREATE_USER = gql`
 	mutation createUser($username: String!, $email: String!, $password: String!) {
 		createUser(username: $username, email: $email, password: $password) {
+			token
 			user {
 				_id
 				username
-				email
-				bookCount
-				saveBooks {
-					authors
-					bookId
-					image
-					link
-					title
-					description
-				}
 			}
-			token
 		}
 	}
 `;
