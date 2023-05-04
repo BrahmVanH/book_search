8aa5236398222071c59205c871962ce20cefc26a
Brahm V <brahmvanh@gmail.com>
2023-05-03 20:14:32 -0400

    Added authorization link including local token ti apolloclient creator in App

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,18 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/temp';
 import Navbar from './components/Navbar';
 
+const authLink = setContext((_, { headers }) => {
+	const token = localStorage.getItem('id_token');
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : '',
+		},
+	};
+});
+
 const client = new ApolloClient({
-	uri: 'http://localhost:3001/graphql',
+	link: authLink.concat(httpLink),
 	cache: new InMemoryCache(),
 });
 
